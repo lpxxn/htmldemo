71a0c7d4603f2dbed5c1bada48137a61b77fbc18
li.p <li.p@angaomeng.com>
2017-07-06 09:52:08 +0800

    package

--- htmltreedemo2/lptree.ts
+++ htmltreedemo2/lptree.ts
@@ -1,9 +1,16 @@
+import {Item, TreeData} from "./treedata";
 /**
  * Created by li on 2017/7/5.
  */
 
 class LpTree {
-
+    containerEle: JQuery;
+    data: Item[];
+    constructor() {
+        this.containerEle = $(".lp_tree");
+        this.data = new TreeData().getData();
+        console.log(this.data);
+    }
 }
 
 
---
+++ tscdemo2/dist/main.js
@@ -0,0 +1,126 @@
+var main =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var Base = (function () {
+    function Base() {
+    }
+    Base.prototype.getName = function () {
+        return "aaa";
+    };
+    return Base;
+}());
+exports.Base = Base;
+//# sourceMappingURL=ibase.js.map
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(0);
+module.exports = __webpack_require__(2);
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var ibase_1 = __webpack_require__(0);
+/**
+ * Created by admin on 2017/6/28.
+ */
+var TTT = (function () {
+    function TTT() {
+        this.Age = 10;
+    }
+    return TTT;
+}());
+exports.TTT = TTT;
+var Test = (function () {
+    function Test() {
+        this.base = new ibase_1.Base();
+        this.base.name = "lili";
+    }
+    Test.prototype.name = function () {
+        return this.base.name;
+    };
+    return Test;
+}());
+exports.Test = Test;
+//# sourceMappingURL=test.js.map
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map
