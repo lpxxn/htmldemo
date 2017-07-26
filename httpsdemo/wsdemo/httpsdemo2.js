4b3e2da065b29749ebae33e0aa5511d8d71f88e2
li.p <li.p@angaomeng.com>
2017-07-26 14:30:46 +0800

    https request

--- httpsdemo/wsdemo/httpsdemo2.js
+++ httpsdemo/wsdemo/httpsdemo2.js
@@ -1,15 +1,16 @@
 var http = require("https");
 var util = require("util");
+var querystring = require('querystring');
 var options = {
     "method": "POST",
-    "hostname": "192.168.0.49",
-    "port": "44319",
+    //"hostname": "192.168.0.49",
+    "hostname": "localhost",
+    "port": "44379",
     "path": "/Login",
     'rejectUnauthorized': false,
     "headers": {
-        "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        "content-type": "application/x-www-form-urlencoded",
         "cache-control": "no-cache",
-        "postman-token": "131af063-3304-c175-a382-79f3e88b460e"
     }
 };
 
@@ -30,8 +31,8 @@ req.on('error', function(e){
     console.error(e);
 });
 
-var data = JSON.stringify({Code: '1000', Pwd: '111111'});
+var data = querystring.stringify({Code: 'admin', Pwd: '111111'});
 
-req.write("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"Code\"\r\n\r\n1000\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"Pwd\"\r\n\r\n111111\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
-//req.write(data);
+//req.write("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"Code\"\r\n\r\nadmin\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"Pwd\"\r\n\r\n111111\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
+req.write(data);
 req.end();
